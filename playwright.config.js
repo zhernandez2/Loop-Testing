1456db9ffe7afda80afb88f235ea23613e8cddac
zhern <zhern@DESKTOP-U2U1NRA>
2024-06-25 19:00:09 -0500

    changed the configs to only test in chromium when using npx playwright test

--- playwright.config.js
+++ playwright.config.js
@@ -37,7 +37,7 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
